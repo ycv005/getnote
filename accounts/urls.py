c01c613e0dfc4d00fea1e549f93e370ddaee1309
ycv005 <ycverma005@gmail.com>
2020-03-24 21:45:44 +0530

    logout, template fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from accounts import urls
-from .views import SignUpView, LoginView
+from .views import SignUpView, LoginView, logout_view
 
 urlpatterns = [
     path("login/",LoginView.as_view(),name='login_page'),
     path("signup/",SignUpView.as_view(),name='signup_page'),
+    path("logout/",logout_view,name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,18 +2,27 @@ from django.shortcuts import render
 from django.views import generic
 from .admin import UserCreationForm
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import forms, logout, login
+from django.http import HttpResponseRedirect
 
 # https://docs.djangoproject.com/en/3.0/ref/class-based-views/generic-editing/#django.views.generic.edit.CreateView
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('accounts:login_view') #checkout below, why using reverse_lazy
+    success_url = reverse_lazy('accounts:login_page') #checkout below, why using reverse_lazy
     template_name = 'accounts/signup.html'
 
 class LoginView(generic.FormView):
-    form_class = AuthenticationForm
-    success_url = reverse_lazy('') #checkout below, why using reverse_lazy
+    form_class = forms.AuthenticationForm
+    success_url = reverse_lazy('home_page') #checkout below, why using reverse_lazy
     template_name = 'accounts/login.html'
+    
+    def form_valid(self,form):
+        login(self.request, form.get_user())
+        return super(LoginView, self).form_valid(form)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 """
 Why to use the reverse_lazy
