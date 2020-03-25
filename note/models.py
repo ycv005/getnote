89c13105032111118e857d6525c3a42c159849fd
ycv005 <ycverma005@gmail.com>
2020-03-25 20:24:09 +0530

    note title

--- note/models.py
+++ note/models.py
@@ -7,11 +7,15 @@ class Note(models.Model):
     uesr = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     text = models.CharField(max_length=1000,null=True,blank=True)
+    # change text's field to TextField from CharField
     # color =
     # pin =
     # tag = 
     # collaborator = 
     created_date = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 # class Images(models.Model):
